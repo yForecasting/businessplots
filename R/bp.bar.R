f32b80605b3414f831ff81e663e4cdd0c8a59448
Yves <yves.sagaert@gmail.com>
2020-02-06 23:51:16 +0100

    Horizontal bar
    add horizontal bar chart to bp.bar

--- R/bp.bar.R
+++ R/bp.bar.R
@@ -1,4 +1,4 @@
-bp.bar <- function(file, valuesgiven=TRUE){
+bp.bar <- function(file, valuesgiven=TRUE, horbars=FALSE){
   file= "YearReport.csv"
   file = "ProductionDataSample.csv"
   df <- read.csv(file,header=TRUE,sep=';')
@@ -8,24 +8,49 @@ bp.bar <- function(file, valuesgiven=TRUE){
   names <- names(df)
   basic_color <- "#999999"
   basic_palette <- "Paired"
-
-  if (valuesgiven){
-  # The values of the bar plot are already calculated
-  ggplot(df, aes(x=df[,1], y=df[,2]))+
-    xlab(names[1])+ylab(names[2])+
-    scale_color_brewer(palette=basic_palette)+
-    theme_minimal() +
-    theme(panel.grid.minor.x = element_blank())+
-    theme(legend.position="none")+
-    geom_bar(color=basic_color,stat="identity")
+  if (horbars){
+    # HORIZONTAL bar chart
+    if (valuesgiven){
+      # The values of the bar plot are already calculated
+      ggplot(df, aes(x=df[,1], y=df[,2]))+
+        xlab(names[1])+ylab(names[2])+
+        scale_color_brewer(palette=basic_palette)+
+        theme_minimal() +
+        theme(panel.grid.minor.x = element_blank())+
+        theme(legend.position="none")+
+        geom_bar(color=basic_color,stat="identity")+
+        coord_flip()
+    } else {
+      # Sample data is given which need to be summarized to bar graph
+      ggplot(df, aes(x=df[,1]))+
+        xlab(names[1])+ylab(names[2])+
+        scale_color_brewer(palette=basic_palette)+
+        theme_minimal() +
+        theme(panel.grid.minor.x = element_blank())+
+        theme(legend.position="none")+
+        geom_bar(color=basic_color)+
+        coord_flip()
+    }
   } else {
-  # Sample data is given which need to be summarized to bar graph
-  ggplot(df, aes(x=df[,1]))+
-    xlab(names[1])+ylab(names[2])+
-    scale_color_brewer(palette=basic_palette)+
-    theme_minimal() +
-    theme(panel.grid.minor.x = element_blank())+
-    theme(legend.position="none")+
-    geom_bar(color=basic_color)
+    # VERTICAL bar chart
+    if (valuesgiven){
+    # The values of the bar plot are already calculated
+    ggplot(df, aes(x=df[,1], y=df[,2]))+
+      xlab(names[1])+ylab(names[2])+
+      scale_color_brewer(palette=basic_palette)+
+      theme_minimal() +
+      theme(panel.grid.minor.x = element_blank())+
+      theme(legend.position="none")+
+      geom_bar(color=basic_color,stat="identity")
+    } else {
+    # Sample data is given which need to be summarized to bar graph
+    ggplot(df, aes(x=df[,1]))+
+      xlab(names[1])+ylab(names[2])+
+      scale_color_brewer(palette=basic_palette)+
+      theme_minimal() +
+      theme(panel.grid.minor.x = element_blank())+
+      theme(legend.position="none")+
+      geom_bar(color=basic_color)
+    }
   }
 }
