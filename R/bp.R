00eb7e5f378252e072be837c750ccbe9e0b64b59
yForecasting <yves.sagaert@gmail.com>
2021-07-05 15:43:41 +0200

    Setting up new version of the basic package via devtools

--- R/bp.R
+++ R/bp.R
@@ -1,3 +1,27 @@
+#' generic businessplot function
+#'
+#' Creates a general plot of the data. 
+#'
+#' This function creates a plot based on the format of the data
+#' and this can be: scatterplot, lineplot or bar plot.
+#' 
+#'
+#' @param file A csv file with the source data
+#'
+#' @author Yves R. Sagaert
+#'
+#' at import forecast
+#' at import Mcomp
+#'
+#' @return A graphical plot
+#' @export
+#'
+#' @examples
+#'   \dontrun{
+#'      bp("testdata.csv")
+#'   }
+#'
+#'
 bp <- function(file){
   df <- read.csv(file,header=TRUE,sep=';')
   if (substr(colnames(df)[1],2,3)== ".."){
---
+++ R/businessplots_package.R
@@ -0,0 +1,9 @@
+#' businessplots: A package for performing basic plots for business
+#'
+#' This package provides functionality for basic business plots
+#' with a standardised way of color scheme.
+#'
+#' @author  Yves R. Sagaert \email{yves.r.sagaert@gmail.com}
+#' @docType package
+#' @name businessplots
+"_PACKAGE"
