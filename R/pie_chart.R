efd6f92a905b3888551d69e79a386fa45cdd99a8
Ruben Vanhecke <ruben.vanhecke@student.vives.be>
2021-07-06 21:49:23 +0200

    Update pie chart and add spider chart

--- R/pie_chart.R
+++ R/pie_chart.R
@@ -24,20 +24,37 @@
 #'
 #'
 
-pieChart <- function(file, main_column){
-  df <- read.csv(file,header=TRUE,sep=';')
-  if (substr(colnames(df)[1],2,3)== ".."){
-    df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
+# plot pie chart
+piechart <- function(file, main_column){
+  # file is the file with all original data to read
+  # main_column is the name of the column with the column names of the pie chart
+
+  # read data
+  data <- read.csv(file,header=TRUE,sep=';')
+  if (substr(colnames(data)[1],2,3)== ".."){
+    data <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
   }
 
-  labels <- names(df)[names(df) != main_column]
-  colors <- c("red","orange","yellow","blue","green")
-  main_values <- df[, main_column]
+  # set labels
+  labels <- names(data)[names(data) != main_column]
 
+  # set colors
+  colors <- c("red","orange","yellow","blue","green") # TODO: read from formatobject
+
+  # set main values
+  main_values <- data[, main_column]
+
+  # plot pie charts
   for (main_value in main_values){
-    values <- df[df[main_column] == main_value][names(df) != main_column]
-    x <- c(strtoi(values[c(1, 2, 3)]), 125)
-    pie(x, labels = labels, col=colors)
+
+    # set values for pie chart
+    values <- data[data[main_column] == main_value][names(data) != main_column]
+
+    # set x for pie chart
+    x <- as.numeric(gsub(",", ".", values))
+
+    # plot pie chart
+    pie(x=x, labels=labels, col=colors)
   }
 }
 
---
+++ R/spiderchart.R
@@ -0,0 +1,67 @@
+#' generic businessplot function
+#'
+#' Creates a general plot of the data.
+#'
+#' This function creates a plot based on the format of the data
+#' and this can be: scatterplot, lineplot or bar plot.
+#'
+#'
+#' @param file A csv file with the source data
+#'
+#' @author Yves R. Sagaert
+#'
+#' at import forecast
+#' at import Mcomp
+#'
+#' @return A graphical plot
+#' @export
+#'
+#' @examples
+#'   \dontrun{
+#'      bp("testdata.csv")
+#'   }
+#'
+#'
+#'
+
+# plot spider chart
+spiderchart <- function(file, main_column, data_column){
+  # file is the file with all original data to read
+  # main_column is the name of the column with the column names of the spider chart
+  # data_column is the name of the column with the data of the spider chart
+
+  # library
+  library(fmsb)
+
+  # read data
+  data <- read.csv(file,header=TRUE,sep=';')
+  if (substr(colnames(df)[1],2,3)== ".."){
+    df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
+  }
+
+  # retrieve columns spider chart
+  spider_columns <- data[, main_column]
+
+  # set empty data frame spider chart
+  spider_data <- data.frame(matrix(ncol=length(spider_columns), nrow=1))
+
+  # set columns empty data frame spider chart
+  colnames(spider_data) <- c(spider_columns)
+
+  # feed empty data frame spider chart with data
+  for (i in 1:nrow(data)){
+    spider_data[i] <- data[i,][data_column]
+  }
+
+  # retrieve max value for spider data
+  max_spider_data <- max(data[, data_column])
+
+  # retrieve min value for spider data
+  min_spider_data <- min(data[, data_column])
+
+  # add max value and min value to spider chart
+  spider_data <- rbind(rep(max_spider_data, min_spider_data) , rep(0, min_spider_data) , spider_data)
+
+  # plot spider chart
+  radarchart(spider_data)
+}
