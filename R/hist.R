f1f1c051f347d21525d31b57432f8d0c74b541a5
Emiel Creus <emiel.creus@student.vives.be>
2021-07-06 11:24:21 +0200

    Histogram Added

---
+++ R/hist.R
@@ -0,0 +1,21 @@
+
+histogram <- function(file){
+  df <- read.csv(file,header=TRUE,sep=';')
+  if (substr(colnames(df)[1],2,3)== ".."){
+    df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
+  }
+  names <- names(df)
+  basic_color <- "#999999"
+  basic_palette <- "Paired"
+
+
+
+  # todo detect sep automatically later ;/,/tab
+  hist(x = df[,2], xlab=names[2], main = "Histogram")
+  curve(dnorm(x, mean=mean(df[,2]), sd=sd(df[,2])), add=TRUE)
+  #scale_linetype_manual(values=c("twodash", "dotted"))
+}
+
+
+
+
