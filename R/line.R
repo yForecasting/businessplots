155e6736bb7700f369b66493777eca8f1ba6908e
Emiel Creus <emiel.creus@student.vives.be>
2021-07-07 13:35:33 +0200

    lineplot optimalization

--- R/line.R
+++ R/line.R
@@ -21,18 +21,17 @@
 #'
 #'
 
-line <- function(file, x_column, y_column, PrimaryColor) {
+line <- function(file, x_column, y_column, symbol, line_type, primary_color, secondary_color) {
   df <- read.csv(file,header=TRUE,sep=';')
   if (substr(colnames(df)[1],2,3)== ".."){
     df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
   }
   names <- names(df)
-  basic_color <- PrimaryColor
   basic_palette <- "Paired"
-  pch <- 1
 
   # todo detect sep automatically later ;/,/tab
-  plot(x=df[,x_column], y=df[,y_column], type ="b", pch = pch, col = basic_color,
+  plot(x=df[,x_column], y=df[,y_column], type = "b", lty=line_type, pch = symbol, col = primary_color,
+       col.axis = secondary_color, col.lab = secondary_color,
        xlab=x_column , ylab = y_column
   )
 
