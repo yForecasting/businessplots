6107456931c1a0f2f308f0dc911044c9ed17c18b
Emiel Creus <emiel.creus@student.vives.be>
2021-07-07 13:26:02 +0200

    scatterplot optimization

--- R/boxplot.R
+++ R/boxplot.R
@@ -1,4 +1,4 @@
-boxplt <- function(file, main_column, mean_red_dot = FALSE){
+boxplt <- function(file, main_column, mean_dot = FALSE, mean_dot_color){
   df <- read.csv(file,header=TRUE,sep=';')
   if (substr(colnames(df)[1],2,3)== ".."){
     df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
@@ -11,8 +11,8 @@ boxplt <- function(file, main_column, mean_red_dot = FALSE){
 
   # todo detect sep automatically later ;/,/tab
   boxplot(x = df[,main_column], xlab=main_column, main = "Boxplot")
-  if(mean_red_dot){
-  points(mean(df[,main_column]), col = "red", pch=19)
+  if(mean_dot){
+  points(mean(df[,main_column]), col = "mean_dot_color", pch=19)
   }
   #scale_linetype_manual(values=c("twodash", "dotted"))
 }
--- R/scatter.R
+++ R/scatter.R
@@ -3,7 +3,7 @@
 #' Creates a scatter plot of the data.
 #'
 #' This function creates a plot based on the format of the data
-#' 
+#'
 #'
 #'
 #' @param file A csv file with the source data, x_colom, a x as, Y_colum, a y as, primaryColor, a primary color for the plot
@@ -23,18 +23,18 @@
 #'
 #'
 
-scatter <- function(file, x_column, y_column, PrimaryColor){
+scatter <- function(file, x_column, y_column, symbol, primary_color, secondary_color){
   df <- read.csv(file,header=TRUE,sep=';')
   if (substr(colnames(df)[1],2,3)== ".."){
     df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
   }
   names <- names(df)
-  basic_color <- PrimaryColor
   basic_palette <- "Paired"
-  pch <- 1
 
   # todo detect sep automatically later ;/,/tab
-  plot(x=df[,x_column], y=df[,y_column], pch = pch, col = basic_color,
+  plot(x=df[,x_column], y=df[,y_column], pch = symbol, col = primary_color,
+       col.axis = secondary_color, col.lab = secondary_color,
     xlab=x_column , ylab = y_column
     )
+
 }
