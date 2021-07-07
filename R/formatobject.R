3dee2e3df01b186ceed73eab982f87bef2a80b50
Emiel Creus <emiel.creus@student.vives.be>
2021-07-07 14:05:47 +0200

    Format object integration

--- R/formatobject.R
+++ R/formatobject.R
@@ -38,28 +38,3 @@ extract_data_formatobject <- function(formatobject = formatobject("formatobject.
   data <- formatobject[, column_name]
   return(as.list(strsplit(data, ",")[[1]]))
 }
-
-##############################################################
-# SAMPLE IMPLEMENTATION - REMOVE BEFORE PRODUCTION RELEASE
-##############################################################
-# you can test the sample by running this file and filling in scatter() in the console. All formatobject lines are marked with comments
-
-# read formatobject
-myObject <- formatobject() # no param = default "formatobject.csv"
-colors <- extract_data_formatobject(formatobject=myObject, "colors") # extract colors formatobject
-
-sample_scatter <- function(file = "YearReport.csv", x_column = "Year", y_column = "Quota"){
-  df <- read.csv(file,header=TRUE,sep=';')
-  if (substr(colnames(df)[1],2,3)== ".."){
-    df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
-  }
-
-  names <- names(df)
-  basic_color <- colors[[1]] # formatobject
-  basic_palette <- "Paired"
-  pch <- 1
-
-  plot(x=df[,x_column], y=df[,y_column], pch = pch, col = basic_color,
-       xlab=x_column , ylab = y_column
-  )
-}
