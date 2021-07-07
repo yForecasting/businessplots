4a7c3fcd17e884ae82f94888ed3adbec799d89e0
Emiel Creus <emiel.creus@student.vives.be>
2021-07-07 12:01:52 +0200

    typo in word secondary

--- R/line.R
+++ R/line.R
@@ -3,14 +3,14 @@
 #' Creates a line plot of the data.
 #'
 #' This function creates a plot based on the format of the data
-#' 
+#'
 #'
 #'
 #' @param file A csv file with the source data
 #' @param x_column a X as, _column a _ as, primaryColor, a primary color for the plot, secondaryColor, a secondary color for the plot
 #' @author Yves R. Sagaert
 #'
-#' 
+#'
 #' @return A line plot
 #' @export
 #'
@@ -21,7 +21,7 @@
 #'
 #'
 
-line <- function(file, x_column, y_column, PrimaryColor, SecondaryColor) {
+line <- function(file, x_column, y_column, PrimaryColor) {
   df <- read.csv(file,header=TRUE,sep=';')
   if (substr(colnames(df)[1],2,3)== ".."){
     df <- read.csv(file,fileEncoding="UTF-8-BOM",header=TRUE,sep=';')
@@ -29,11 +29,10 @@ line <- function(file, x_column, y_column, PrimaryColor, SecondaryColor) {
   names <- names(df)
   basic_color <- PrimaryColor
   basic_palette <- "Paired"
-  color <- SecundaryColor
   pch <- 1
 
   # todo detect sep automatically later ;/,/tab
-  plot(x=df[,x_column], y=df[,y_column], type ="b", pch = pch, col = color,
+  plot(x=df[,x_column], y=df[,y_column], type ="b", pch = pch, col = basic_color,
        xlab=x_column , ylab = y_column
   )
 
